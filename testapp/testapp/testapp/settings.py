67b081fd91fa17b358de68854266353c3f75195d
Abhijit Bangera <abhijit.bangera@hotmail.com>
2017-10-17 12:59:39 +0530

    edit procfile

--- testapp/testapp/testapp/settings.py
+++ testapp/testapp/testapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+6!#k%3t^$afq009(m&)r+&u0b_=0+ncz#csx$chql0jriw_qw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [test23443.herokuapp.com]
 
 
 # Application definition
